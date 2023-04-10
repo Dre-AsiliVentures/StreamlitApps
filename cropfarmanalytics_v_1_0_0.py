1e923bc48c6966cfc00a418825f2b7bb30bacff0
ElectronicsDr <61284769+ElectronicsDr@users.noreply.github.com>
2023-04-10 16:47:40 +0300

    Update cropdataAnalytics v1.0.2
    Added the Histogram from Matplotlib

--- cropfarmanalytics_v_1_0_0.py
+++ cropfarmanalytics_v_1_0_0.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Load crop data from CSV file
 csv_url= "https://raw.githubusercontent.com/ElectronicsDr/StreamlitApps/main/crop_data.csv"
@@ -26,8 +27,14 @@ yield_samples = np.random.normal(yield_mean, yield_std, size=n_samples)
 fert_samples = np.random.normal(fert_mean, fert_std, size=n_samples)
 
 # Display histogram of yield and fertilizer values
-st.subheader('Yield histogram')
-st.hist(yield_samples)
+fig, ax = plt.subplots()
+ax.hist(yield_samples)
+ax.set_xlabel('Yield')
+ax.set_ylabel('Frequency')
+st.pyplot(fig)
 
-st.subheader('Fertilizer histogram')
-st.hist(fert_samples)
+fig, ax = plt.subplots()
+ax.hist(fert_samples)
+ax.set_xlabel('Fertilizer')
+ax.set_ylabel('Frequency')
+st.pyplot(fig)
