2c525258e66da0beec7ffbd7ac1a3d3c1a90a492
ElectronicsDr <61284769+ElectronicsDr@users.noreply.github.com>
2023-04-19 14:02:21 +0300

    Created Irrigation Dashboard Yield Metrics v1.py
    Created Irrigation Dashboard Yield Metrics v1.0.0. This script presents the model metrics for a farm with the Crop Yield per acre, Water Use Efficiency, Water Productivity, Irrigation Efficiency and the Gross Margin.

---
+++ irrigationdashboard_yieldmetrics_v1_0_0.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+
+# Define sample farm size
+farm_size = 1000  # in acres
+
+# Generate sample data for crop yield
+crop_yield = np.random.normal(loc=10, scale=2, size=farm_size)
+crop_yield_data = pd.DataFrame({'Crop Yield': crop_yield})
+crop_yield_data.to_csv('crop_yield.csv', index=False)
+
+# Generate sample data for water use efficiency
+water_use = np.random.normal(loc=20, scale=5, size=farm_size)
+crop_yield = crop_yield * crop_yield.max() / crop_yield.mean()
+water_use_efficiency = crop_yield / water_use
+water_use_data = pd.DataFrame({'Water Use Efficiency': water_use_efficiency})
+water_use_data.to_csv('water_use.csv', index=False)
+
+# Generate sample data for water productivity
+water_productivity = crop_yield * crop_yield.max() / (water_use * 1000)  # assume 1000 liters of water per acre
+water_prod_data = pd.DataFrame({'Water Productivity': water_productivity})
+water_prod_data.to_csv('water_productivity.csv', index=False)
+
+# Generate sample data for irrigation efficiency
+irrigation_efficiency = np.random.normal(loc=0.8, scale=0.1, size=farm_size)
+irrigation_eff_data = pd.DataFrame({'Irrigation Efficiency': irrigation_efficiency})
+irrig_eff_data.to_csv('irrigation_efficiency.csv', index=False)
+
+# Generate sample data for gross margin
+crop_value = crop_yield * 2  # assume a crop value of $2 per unit
+water_cost = water_use * 0.5  # assume a water cost of $0.5 per unit
+labor_cost = np.random.normal(loc=5, scale=1, size=farm_size)  # assume labor cost of $5 per acre
+gross_margin = crop_value - water_cost - labor_cost
+gross_margin_data = pd.DataFrame({'Crop': range(1, farm_size+1), 'Gross Margin': gross_margin})
+gross_margin_data.to_csv('gross_margin.csv', index=False)
