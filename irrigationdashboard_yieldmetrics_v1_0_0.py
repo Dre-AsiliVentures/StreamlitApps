97567fb557dedef69075cb72ca7469034ddb73fb
ElectronicsDr <61284769+ElectronicsDr@users.noreply.github.com>
2023-04-25 12:46:35 +0300

    Update irrigationdashboard_yieldmetrics_v1_0_0.py

--- irrigationdashboard_yieldmetrics_v1_0_0.py
+++ irrigationdashboard_yieldmetrics_v1_0_0.py
@@ -1,38 +1,42 @@
+import streamlit as st
 import numpy as np
-import pandas as pd
 
-# Define sample farm size
-farm_size = 1000  # in acres
+# Define a dictionary of crops and their yield ranges
+crops = {
+    "Wheat": (2000, 5000),
+    "Corn": (3000, 8000),
+    "Rice": (4000, 10000)
+}
 
-# Generate sample data for crop yield
-crop_yield = np.random.normal(loc=10, scale=2, size=farm_size)
-crop_yield_data = pd.DataFrame({'Crop Yield': crop_yield})
-crop_yield_data.to_csv('crop_yield.csv', index=False)
+# Define a function to generate random values for the selected crop
+def generate_values(crop):
+    yield_range = crops[crop]
+    crop_yield = np.random.randint(yield_range[0], yield_range[1]+1, size=1)[0]  # kg/ha
+    water_use_efficiency = np.random.uniform(1, 4, size=1)[0]  # kg/m3
+    water_productivity = np.random.randint(2, 10, size=1)[0]  # kg/m3
+    irrigation_efficiency = np.random.uniform(50, 90, size=1)[0]  # %
+    gross_margin = np.random.randint(1000, 5000, size=1)[0]  # USD/ha
+    return crop_yield, water_use_efficiency, water_productivity, irrigation_efficiency, gross_margin
 
-# Generate sample data for water use efficiency
-water_use = np.random.normal(loc=20, scale=5, size=farm_size)
-crop_yield = crop_yield * crop_yield.max() / crop_yield.mean()
-water_use_efficiency = crop_yield / water_use
-water_use_data = pd.DataFrame({'Water Use Efficiency': water_use_efficiency})
-water_use_data.to_csv('water_use.csv', index=False)
+# Set up the Streamlit app
+st.title("Irrigation Dashboard")
+st.header("Select a Crop")
+selected_crop = st.selectbox("Select a crop", list(crops.keys()))
 
-# Generate sample data for water productivity
-water_productivity = crop_yield * crop_yield.max() / (water_use * 1000)  # assume 1000 liters of water per acre
-water_prod_data = pd.DataFrame({'Water Productivity': water_productivity})
-water_prod_data.to_csv('water_productivity.csv', index=False)
+# Generate the values for the selected crop
+crop_yield, water_use_efficiency, water_productivity, irrigation_efficiency, gross_margin = generate_values(selected_crop)
 
-# Generate sample data for irrigation efficiency
-irrigation_efficiency = np.random.normal(loc=0.8, scale=0.1, size=farm_size)
-irrigation_eff_data = pd.DataFrame({'Irrigation Efficiency': irrigation_efficiency})
-irrig_eff_data.to_csv('irrigation_efficiency.csv', index=False)
-
-# Generate sample data for gross margin
-crop_value = crop_yield * 2  # assume a crop value of $2 per unit
-water_cost = water_use * 0.5  # assume a water cost of $0.5 per unit
-labor_cost = np.random.normal(loc=5, scale=1, size=farm_size)  # assume labor cost of $5 per acre
-gross_margin = crop_value - water_cost - labor_cost
-gross_margin_data = pd.DataFrame({'Crop': range(1, farm_size+1), 'Gross Margin': gross_margin})
-gross_margin_data.to_csv('gross_margin.csv', index=False)
-
-#In this example code, we use NumPy to generate sample data for each of the five metrics (crop yield, water use efficiency, water productivity, irrigation efficiency, and gross margin) based on a farm size of 1000 acres. The generated data is stored in individual CSV files.
-#Note that this is just an example code, and you would need to adjust the parameters (such as mean and standard deviation) and assumptions (such as crop value and water cost) to match your specific farm and irrigation management practices.
+# Display the selected crop and its generated values
+st.header(selected_crop)
+st.write("- Crop yield (kg/ha)")
+st.write("- Water use efficiency (kg/m3)")
+st.write("- Water productivity (kg/m3)")
+st.write("- Irrigation efficiency (%)")
+st.write("- Gross margin (USD/ha)")
+st.write("")
+st.write("Randomly Generated Values:")
+st.write(f"- Crop yield: {crop_yield} kg/ha")
+st.write(f"- Water use efficiency: {water_use_efficiency:.2f} kg/m3")
+st.write(f"- Water productivity: {water_productivity} kg/m3")
+st.write(f"- Irrigation efficiency: {irrigation_efficiency:.2f}%")
+st.write(f"- Gross margin: {gross_margin} USD/ha")
