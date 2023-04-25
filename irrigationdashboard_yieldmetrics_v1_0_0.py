134d5f98ddc4c0e715d050d40e09e837cb47aae3
ElectronicsDr <61284769+ElectronicsDr@users.noreply.github.com>
2023-04-25 14:25:06 +0300

    Update irrigationdashboard_yieldmetrics_v1_0_0.py

--- irrigationdashboard_yieldmetrics_v1_0_0.py
+++ irrigationdashboard_yieldmetrics_v1_0_0.py
@@ -1,60 +1,25 @@
-# Import necessary packages
 import streamlit as st
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Generate random data
-years = np.arange(2010, 2023)
-interval_values = np.random.choice([3, 7, 14, 21], size=len(years))
-depth_values = np.random.randint(50, 200, size=len(years))
-yield_values = np.round(np.random.uniform(1, 4, size=len(years)), decimals=2)
-wue_values = np.round(np.random.uniform(1, 3, size=len(years)), decimals=2)
-wp_values = np.round(np.random.uniform(0.5, 2, size=len(years)), decimals=2)
-efficiency_values = np.round(np.random.uniform(0.5, 1, size=len(years)), decimals=2)
-margin_values = np.round(np.random.uniform(2000, 6000, size=len(years)), decimals=2)
+# Set page title
+st.set_page_config(page_title="My Streamlit App")
 
-# Combine data into a Pandas DataFrame
-data = pd.DataFrame({
-    'Year': years,
-    'Irrigation Interval (days)': interval_values,
-    'Irrigation Application Depth (mm)': depth_values,
-    'Crop Yield per Acre': yield_values,
-    'Water Use Efficiency (WUE)': wue_values,
-    'Water Productivity (WP)': wp_values,
-    'Irrigation Efficiency': efficiency_values,
-    'Gross Margin': margin_values
-})
+# Set page header
+st.header("My Streamlit App")
 
-# Create the dashboard
-st.title('Irrigation Metrics Dashboard')
+# Set slider for selecting a value
+x_min = st.slider("Select x_min", -10.0, 10.0, -5.0)
+x_max = st.slider("Select x_max", -10.0, 10.0, 5.0)
 
-# Display the data
-st.write(data)
+# Generate some data
+x = np.linspace(x_min, x_max, 100)
+y = np.sin(x)
 
-# Create a sidebar for filtering the data
-interval = st.sidebar.slider('Select Irrigation Interval (days)', 3, 21, 7, step=1)
-depth = st.sidebar.slider('Select Irrigation Application Depth (mm)', 50, 200, 100, step=10)
-
-# Filter the data based on the selected interval and depth
-filtered_data = data[(data['Irrigation Interval (days)'] == interval) & (data['Irrigation Application Depth (mm)'] == depth)]
-
-# Display the filtered data
-st.write(filtered_data)
-
-# Create a bar chart to display the metrics
-metrics = ['Crop Yield per Acre', 'Water Use Efficiency (WUE)', 'Water Productivity (WP)', 'Irrigation Efficiency', 'Gross Margin']
-selected_metric = st.selectbox('Select a Metric', metrics)
-
-# Filter the data based on the selected metric
-metric_data = filtered_data[['Year', selected_metric]]
-
-# Set the Year column as the index
-metric_data = metric_data.set_index('Year')
-
-# Create a bar chart
+# Plot the data
 fig, ax = plt.subplots()
-metric_data.plot(kind='bar', ax=ax)
-ax.set_xlabel('Year')
-ax.set_ylabel(selected_metric)
+ax.plot(x, y)
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_title("Sine Wave")
 st.pyplot(fig)
