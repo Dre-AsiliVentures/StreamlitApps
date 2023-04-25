46b4176abe8f320eca154c9d0abcb898246ae3f6
ElectronicsDr <61284769+ElectronicsDr@users.noreply.github.com>
2023-04-25 17:05:13 +0300

    Create irrigationdashboard_yieldmetrics_v1_1_0.py
    Line 12: Kc changed to "Kc Ini" as per Sheet details
    Line 17: Commented yield_coeff = crop_params['yield_coeff']. Yield_coeff is now a constant value of 0.35
    Line 18: Commented price = crop_params['price'], Price is now a constant value of 300

---
+++ irrigationdashboard_yieldmetrics_v1_1_0.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import streamlit as st
+
+# Read crop details from Excel file
+crop_details = pd.read_excel('https://asiliventures.com/wp-content/uploads/2023/04/FYR-07-DATABASE-ANALYTICS-COMPLETE.xlsx', sheet_name='Crop Details')
+
+# Extract crop names and Kc values
+CROP_TYPES = {}
+for i, row in crop_details.iterrows():
+    crop_name = row['Crop Name']
+    kc = row['Kc Ini']
+    CROP_TYPES[crop_name] = {'kc': Kc ini, 'yield_coeff': 3.5, 'price': 300}
+
+def compute_irrigation_info(crop_type, irrigation_interval, gross_irrigation_depth):
+    crop_params = CROP_TYPES[crop_type]
+    kc = crop_params['kc']
+    #yield_coeff = crop_params['yield_coeff']
+    yield_coeff=0.35
+    yield_coeff = crop_params['yield_coeff']
+    #price = crop_params['price']
+    price=300
+
+    depth_values = [gross_irrigation_depth / 2, gross_irrigation_depth / 2]
+    interval_values = [irrigation_interval, irrigation_interval]
+
+    water_applied = sum(depth_values) * kc
+    yield_per_ha = yield_coeff * water_applied
+    wue = yield_per_ha / sum(depth_values)
+    wp = yield_per_ha / (sum(depth_values) * price)
+    irrigation_efficiency = sum(depth_values) / (sum(depth_values) + water_applied)
+
+    gross_margin = yield_per_ha * price
+
+    return {
+        'Irrigation Interval (days)': interval_values,
+        'Irrigation Application Depth (mm)': depth_values,
+        'Crop Yield per Acre (kg/ha)': yield_per_ha,
+        'Water Use Efficiency (WUE) in percentage': wue * 100,
+        'Water Productivity (WP)': wp,
+        'Irrigation Efficiency in percentage': irrigation_efficiency * 100,
+        'Gross Margin (Dollars)': gross_margin
+    }
+
+st.sidebar.title('Irrigation Demo Calculator')
+
+crop_type = st.sidebar.selectbox('Crop type', list(CROP_TYPES.keys()))
+irrigation_interval = st.sidebar.slider('Irrigation Interval (days)', 1, 30, 7)
+gross_irrigation_depth = st.sidebar.slider('Gross Irrigation Depth (mm)', 100, 500, 250)
+
+results = compute_irrigation_info(crop_type, irrigation_interval, gross_irrigation_depth)
+
+st.title(f'Irrigation Information for {crop_type}')
+st.write(results)
