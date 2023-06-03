29d070307b2dcfc759941d9c0975d7282f3075a5
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-06-03 09:02:11 +0300

    Update seedrateconverter.py

--- seedrateconverter.py
+++ seedrateconverter.py
@@ -11,6 +11,14 @@ def convert_seed_rate(seed_rate, from_unit, to_unit):
         return seed_rate * 2.47105
     elif from_unit == "seeds/acre" and to_unit == "seeds/ha":
         return seed_rate / 2.47105
+    elif from_unit == "kg/ha" and to_unit == "kg/acre":
+        return seed_rate * 0.404686
+    elif from_unit == "kg/acre" and to_unit == "kg/ha":
+        return seed_rate / 0.404686
+    elif from_unit == "lbs/ha" and to_unit == "lbs/acre":
+        return seed_rate * 0.892179
+    elif from_unit == "lbs/acre" and to_unit == "lbs/ha":
+        return seed_rate / 0.892179
     else:
         return seed_rate
 
@@ -19,8 +27,8 @@ def main():
     st.title("Seed Rate Converter")
     
     seed_rate = st.number_input("Enter seed rate:")
-    from_unit = st.selectbox("Convert from:", ["seeds/ha", "seeds/acre"])
-    to_unit = st.selectbox("Convert to:", ["seeds/ha", "seeds/acre"])
+    from_unit = st.selectbox("Convert from:", ["seeds/ha", "seeds/acre", "kg/ha", "kg/acre", "lbs/ha", "lbs/acre"])
+    to_unit = st.selectbox("Convert to:", ["seeds/ha", "seeds/acre", "kg/ha", "kg/acre", "lbs/ha", "lbs/acre"])
 
     converted_rate = convert_seed_rate(seed_rate, from_unit, to_unit)
 
@@ -28,3 +36,4 @@ def main():
     
 if __name__ == "__main__":
     main()
+
